90fdf2b6df27f7003d4bdbf8f5da3def354549c3
Roman Mahiiovych <romanmah1998@gmail.com>
2019-10-11 11:32:25 +0300

    Create models Role, table roles, table roles_user, seeder RolesSeeder

---
+++ app/Models/Role.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    public $timestamps = false;
+    protected $table = 'roles';
+
+    protected $fillable = [
+        'name', 'slug', 'permissions'
+    ];
+
+    protected $casts = [
+        'permissions' => 'array'
+    ];
+}
---
+++ database/seeds/RolesSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $author = Role::create([
+            'name' => 'Author',
+            'slug' => 'author',
+            'permissions' => [
+                'create-post' => true,
+                'create-comment' => true
+            ]
+        ]);
+
+        $commentator = Role::create([
+            'name' => 'Commentator',
+            'slug' => 'commentator',
+            'permissions' => [
+                'create-comment' => true
+            ]
+        ]);
+    }
+}
